0684ea0f56bfefd2ce82b7ef9ec53d4764267adc
Salmon168 <66323212+Salmon168@users.noreply.github.com>
2022-03-31 22:37:53 +1100

    partially added audio

--- src/WordDisplay.js
+++ src/WordDisplay.js
@@ -2,7 +2,15 @@ import React from "react";
 
 export default function WordDisplay(props) {
   if (props.wordinfo) {
-    return <div>Hallo from word</div>;
+    let audio = props.wordinfo[0].hwi.prs[0].sound.audio;
+    let subdirectory = audio.slice(0, 1);
+    let audioUrl = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${subdirectory}/${audio}.mp3`;
+    return (
+      <div>
+        <h1>{props.wordinfo[0].meta.id}</h1>
+        <span>{props.wordinfo[0].hwi.prs[0].mw}</span>
+      </div>
+    );
   } else {
     return null;
   }
