d041cc78c460d0e69c564011955223854d7e7bf8
Salmon168 <66323212+Salmon168@users.noreply.github.com>
2022-04-01 00:53:15 +1100

    Added audio of pronounciation

--- src/WordDisplay.js
+++ src/WordDisplay.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Howl } from "howler";
 
 export default function WordDisplay(props) {
+  let [play, setPlay] = useState(null);
   if (props.wordinfo) {
     let audio = props.wordinfo[0].hwi.prs[0].sound.audio;
     let subdirectory = audio.slice(0, 1);
     let audioUrl = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${subdirectory}/${audio}.mp3`;
+
+    function playAudio(event) {
+      event.preventDefault();
+      var sound = new Howl({
+        src: [audioUrl],
+        html5: true,
+      });
+
+      sound.play();
+    }
+
     return (
-      <div>
+      <div classsName="WordDisplay">
         <h1>{props.wordinfo[0].meta.id}</h1>
         <span>{props.wordinfo[0].hwi.prs[0].mw}</span>
+        <div>
+          <button onClick={playAudio}>🔉</button>
+        </div>
       </div>
     );
   } else {
